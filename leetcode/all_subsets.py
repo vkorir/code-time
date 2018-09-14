edc4e422729dc4212f1ff7efabb70dcf8d15bf62
Victor Korir <vkkorir@gmail.com>
2018-09-13 22:22:53 -0700

    updated all_subsets

--- leetcode/all_subsets.py
+++ leetcode/all_subsets.py
@@ -1,32 +1,28 @@
-"""
-[1 2 3] => [[1] [1 2] [1 2 3] [1 3] [2] [2 3] [3]]
-"""
+import unittest
 
-
-def all_subsets(arr):
-    res = []
-    for i in range(len(arr)):
-        l = 1
-        while i + l <= len(arr):
-            res.append(arr[i: i + l])
-            l += 1
-    return res
-
+class Test(unittest.TestCase):
+    """
+    [1 2 3] => [[1] [1 2] [1 2 3] [1 3] [2] [2 3] [3]]
+    """
+    def test(self):
+        arr = [1, 2, 3]
+        actual = subsets(arr)
+        expected = [[1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]]
+        self.assertEqual(actual, expected)
 
 def subsets(arr):
     subset, res = [], []
     find_subsets(arr, 0, subset, res)
     return res
 
-
 def find_subsets(arr, index, subset, res):
     for i in range(index, len(arr)):
         subset.append(arr[i])
         res.append(subset[:])
         find_subsets(arr, i + 1, subset, res)
-        subset.pop(-1)
+        subset.pop()
     return
 
-
-print(subsets([1, 2, 3]))
+if __name__ == '__main__':
+    unittest.main()
 
